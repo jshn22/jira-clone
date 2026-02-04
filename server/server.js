4338ea4f97aa55732c062e8f697a9e27b0242c64
Jashanpreet Kaur <jashanpreetkaur24@navgurukul.org>
2026-02-04 15:34:17 +0530

    server.js changed

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const app = express();
 
 // Middleware
 const corsOptions = {
-  origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  origin: process.env.FRONTEND_URL || "https://jira-clone-frontend-o45a.onrender.com",
   credentials: true,
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
